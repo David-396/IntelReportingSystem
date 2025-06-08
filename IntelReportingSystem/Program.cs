dc5770208297aa7d8c6efc1cf01df70f61af7497
David-396 <david.eini12@gmail.com>
2025-06-08 19:10:55 +0300

    Add project files.

---
+++ IntelReportingSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+}
